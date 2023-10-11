3b45e603463b7c81683fc0ea356a8a139bbea6bc
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-10-10 22:31:35 -0700

    Changed name of game

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Coffee Clicker";
 
 document.title = gameName;
 
