c9aa0146bbf9b4004926fe8adb399f84edb4b859
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-10-10 23:46:02 -0700

    Added button

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,11 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <div class="card">
+        <button name="button">Press me</button>
+    </div>
+  </div>
+`;
