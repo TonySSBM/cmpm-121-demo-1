e90291b93d65588fc9075764fdb2f37f16a89b2a
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-10-11 01:37:24 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,27 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+let counter: number = 0;
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <div class="card">
-        <button name="button">☕</button>
+    <button id="counter" button name="button">☕</button>
+    </div>
+    <div id="panel">
+      <div id="field_name">${counter} coffees</div>
     </div>
   </div>
 `;
+
+document
+  .getElementById("counter")!
+  .addEventListener("click", () => (counter += 1));
+document
+  .getElementById("counter")!
+  .addEventListener(
+    "click",
+    () =>
+      (document.getElementById("field_name")!.textContent =
+        counter + " coffees"),
+  );
