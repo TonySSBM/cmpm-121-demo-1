ee1534a17837ab14f3bdb10667f733745de439d1
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-10-12 04:35:11 -0700

    step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,9 @@ app.append(header);
 let coffees: number = 0;
 let autoNum: number = 0;
 let pastTime: number = 0;
-const purchaseCosts: number[] = [10];
-const purchaseIncrease: number[] = [1];
+const purchaseCosts: number[] = [10, 100, 1000];
+const purchaseIncrease: number[] = [0.1, 2.0, 50];
+const purchaseAmounts: number[] = [1, 1, 1];
 
 function autoCoffee(numPick: number) {
   shopEnabling();
@@ -29,6 +30,27 @@ function autoCoffee(numPick: number) {
 function purchase(purchaseObj: number) {
   coffees -= purchaseCosts[purchaseObj];
   autoNum += purchaseIncrease[purchaseObj];
+  purchaseAmounts[purchaseObj] += 1;
+  document.getElementById("autoClick")!.textContent =
+    autoNum.toFixed(2) + " coffees per second";
+  document.getElementById("purchaseZero")!.textContent =
+    "Coffee Grinder " +
+    purchaseAmounts[0] +
+    ": costs " +
+    purchaseCosts[0] +
+    " coffees";
+  document.getElementById("purchaseOne")!.textContent =
+    "Coffee Kettle " +
+    purchaseAmounts[1] +
+    ": costs " +
+    purchaseCosts[1] +
+    " coffees";
+  document.getElementById("purchaseTwo")!.textContent =
+    "Espresso Machine " +
+    purchaseAmounts[2] +
+    ": costs " +
+    purchaseCosts[2] +
+    " coffees";
 }
 
 requestAnimationFrame(function () {
@@ -38,6 +60,10 @@ requestAnimationFrame(function () {
 function shopEnabling() {
   (document.getElementById("purchaseZero") as HTMLButtonElement)!.disabled =
     coffees < purchaseCosts[0];
+  (document.getElementById("purchaseOne") as HTMLButtonElement)!.disabled =
+    coffees < purchaseCosts[1];
+  (document.getElementById("purchaseTwo") as HTMLButtonElement)!.disabled =
+    coffees < purchaseCosts[2];
 }
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
@@ -47,10 +73,17 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </div>
     <div id="panel">
       <div id="field_name">${coffees.toFixed(2)} coffees made</div>
+      <div id="autoClick">${autoNum.toFixed(2)} coffees per second</div>
     </div>
     <div class="card">
-      <button id="purchaseZero" button name="button">Coffee Grinder: costs ${
+      <button id="purchaseZero" button name="button">Coffee Grinder 1: costs ${
         purchaseCosts[0]
+      } coffees</button>
+      <button id="purchaseOne" button name="button">Coffee Kettle 1: costs ${
+        purchaseCosts[1]
+      } coffees</button>
+      <button id="purchaseTwo" button name="button">Espresso Machine 1: costs ${
+        purchaseCosts[2]
       } coffees</button>
     </div>
   </div>
@@ -73,3 +106,11 @@ document
 document
   .getElementById("purchaseZero")!
   .addEventListener("click", () => purchase(0));
+
+document
+  .getElementById("purchaseOne")!
+  .addEventListener("click", () => purchase(1));
+
+document
+  .getElementById("purchaseTwo")!
+  .addEventListener("click", () => purchase(2));
