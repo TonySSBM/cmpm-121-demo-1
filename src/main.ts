b92d4c26ffa9e8294dc1ec493bf881c3e08c144a
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-10-12 02:46:33 -0700

    step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,22 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-let counter: number = 0;
+let coffees: number = 0;
 const autoNum: number = 1; //change to let later
+let pastTime: number = 0;
 
-function autoCoffee(autoNum: number) {
-  counter += autoNum;
-  document.getElementById("field_name")!.textContent = counter + " coffees";
+function autoCoffee(numPick: number) {
+  numPick = autoNum;
+  coffees += (numPick * (performance.now() - pastTime)) / 1000;
+  pastTime = performance.now();
+  document.getElementById("field_name")!.textContent =
+    coffees.toFixed(2) + " coffees made";
+  requestAnimationFrame(autoCoffee);
 }
+
+requestAnimationFrame(function () {
+  autoCoffee(autoNum);
+});
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -24,18 +33,14 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <button id="counter" button name="button">☕</button>
     </div>
     <div id="panel">
-      <div id="field_name">${counter} coffees</div>
+      <div id="field_name">${coffees.toFixed(2)} coffees made</div>
     </div>
   </div>
 `;
 
-setInterval(function () {
-  autoCoffee(autoNum);
-}, 1000);
-
 document
   .getElementById("counter")!
-  .addEventListener("click", () => (counter += 1));
+  .addEventListener("click", () => (coffees += 1));
 document;
 
 document
@@ -44,5 +49,5 @@ document
     "click",
     () =>
       (document.getElementById("field_name")!.textContent =
-        counter + " coffees"),
+        coffees.toFixed(2) + " coffees made"),
   );
