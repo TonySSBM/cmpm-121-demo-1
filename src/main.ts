d86561396ed3091ed67fc8bb793ae0ee4dbae0f5
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-10-12 03:14:55 -0700

    step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,13 @@ header.innerHTML = gameName;
 app.append(header);
 
 let coffees: number = 0;
-const autoNum: number = 1; //change to let later
+let autoNum: number = 0;
 let pastTime: number = 0;
+const purchaseCosts: number[] = [10];
+const purchaseIncrease: number[] = [1];
 
 function autoCoffee(numPick: number) {
+  shopEnabling();
   numPick = autoNum;
   coffees += (numPick * (performance.now() - pastTime)) / 1000;
   pastTime = performance.now();
@@ -23,17 +26,32 @@ function autoCoffee(numPick: number) {
   requestAnimationFrame(autoCoffee);
 }
 
+function purchase(purchaseObj: number) {
+  coffees -= purchaseCosts[purchaseObj];
+  autoNum += purchaseIncrease[purchaseObj];
+}
+
 requestAnimationFrame(function () {
   autoCoffee(autoNum);
 });
 
+function shopEnabling() {
+  (document.getElementById("purchaseZero") as HTMLButtonElement)!.disabled =
+    coffees < purchaseCosts[0];
+}
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <div class="card">
-    <button id="counter" button name="button">☕</button>
+      <button id="counter" button name="button">☕</button>
     </div>
     <div id="panel">
       <div id="field_name">${coffees.toFixed(2)} coffees made</div>
+    </div>
+    <div class="card">
+      <button id="purchaseZero" button name="button">Coffee Grinder: costs ${
+        purchaseCosts[0]
+      } coffees</button>
     </div>
   </div>
 `;
@@ -51,3 +69,7 @@ document
       (document.getElementById("field_name")!.textContent =
         coffees.toFixed(2) + " coffees made"),
   );
+
+document
+  .getElementById("purchaseZero")!
+  .addEventListener("click", () => purchase(0));
